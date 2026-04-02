3bf4ebead8a1133a2f8b3542a3261a18458a3890
bhavyakuamwat-code <bhavyakuamwat@gmail.com>
2026-04-02 11:37:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// TEST ROUTE
+app.get('/', (req, res) => {
+  res.send('Server is running 🚀');
+});
+
 // 0. ADMIN LOGIN
 app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
